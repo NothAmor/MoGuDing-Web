4949a23b2fc8cc161e395f6d72c0401ea032a9cb
NothAmor <i@nothamor.cn>
2021-12-09 18:34:18 +0800

    test

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app.cron.cron import cronMethod
 from config import flaskConfig
-
+ 
 if __name__ == '__main__':
     flaskConfig.scheduler.add_job(func=cronMethod.refreshJobs, trigger='cron', second='*/5', id='refresh')
     flaskConfig.scheduler.start()
